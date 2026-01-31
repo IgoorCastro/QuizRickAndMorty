638b17c0cb0ab6f3569982f3f258d8086db1b4a8
IgoorCastro <igor_.contato@hotmail.com>
2026-01-31 16:16:22 -0300

    style(options): melhoria na UI e UX

--- app/components/quiz/Options.tsx
+++ app/components/quiz/Options.tsx
@@ -11,6 +11,7 @@ type OptionsProps = {
 // callback retorna o nome do character selecionado (para compração)
 // quando montada, handler recebe uma função com o parametro do nome
 // do personagem, facilitando a comparação
+// verificar o loading
 export default function Options({ characters, onClick, isLoading }: OptionsProps) {
     return (
         <div className="w-full h-auto flex flex-col md:flex-row justify-center items-center gap-3">
